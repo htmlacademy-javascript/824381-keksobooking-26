cb0ed381a65bc57676c5c43ee52f6d2665f2e743
antondubovskiy <dubovskiy@mybook.ru>
2022-06-07 13:10:30 +0300

    function names corrections

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 //Random digit helper
-function getRandomNumer(min, max) {
+function getRandomNumber(min, max) {
   return min < max && min >= 0
     ? Math.round(Math.random() * (max - min) + min)
     : console.log("wrong number range");
 }
-getFractionNumer(0, 3);
+getRandomNumber(0, 3);
 
 //Random fraction digit helper
-function getFractionNumer(min, max, fractionLength) {
+function getFractionNumber(min, max, fractionLength) {
   return min < max && min >= 0
     ? Number((Math.random() * (max - min) + min).toFixed(fractionLength))
     : console.log("wrong number range");
 }
 
-getFractionNumer(1.1, 1.2, 2);
+getFractionNumber(1.1, 1.2, 2);

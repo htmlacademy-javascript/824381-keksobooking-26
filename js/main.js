375dbfb64a7303271196132f53734c167b1f0985
antondubovskiy <dubovskiy@mybook.ru>
2022-06-08 14:06:52 +0300

    numbers-functions updated and jsdoc files added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-//Random digit helper
-function getRandomNumber(min, max) {
-  return min < max && min >= 0
-    ? Math.round(Math.random() * (max - min) + min)
-    : false;
-}
+/**
+ * Helper function that returns random number from specified range value
+ * @param {*} min - min range value
+ * @param {*} max - max range value
+ * @returns - random number
+ * @example
+ * getRandomNumber(1, 3);
+ * // => 2
+ */
+const getRandomNumber = (min, max) =>
+  min < max && min >= 0 ? Math.round(Math.random() * (max - min) + min) : false;
 getRandomNumber(0, 3);
 
-//Random fraction digit helper
-function getFractionNumber(min, max, fractionLength) {
-  return min < max && min >= 0
-    ? Number((Math.random() * (max - min) + min).toFixed(fractionLength))
-    : false;
-}
-
+/**
+ * Helper function that returns random fraction number from specified range value
+ * @param {*} min - min range value
+ * @param {*} max - max range value
+ * @param {*} fractionLength - number of digits after the fraction point
+ * @returns - random fraction number
+ * @example
+ * getFractionNumber(1.1, 1.2, 2);
+ * // => 1.12
+ */
+const getFractionNumber = (min, max, fractionLength) =>
+  min < max && min >= 0 ? Number((Math.random() * (max - min) + min).toFixed(fractionLength)) : false;
 getFractionNumber(1.1, 1.2, 2);

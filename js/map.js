df6d88dfd08ba9dee3797512bc0960e348449336
Anton Dubovskiy <a.n.dubovskiy@gmail.com>
2022-07-07 19:26:09 +0300

    map module fix

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
-import { enableForms } from "./form.js";
-import { getSimilarAd } from "./data.js";
-import { generateAd } from "./ad-generator.js";
+import { enableForms } from './form.js';
+import { getSimilarAd } from './data.js';
+import { generateAd } from './ad-generator.js';
 
 /**
  * Variables for map and form update
  */
 const randomData = getSimilarAd();
-const adressInput = document.querySelector("#address");
-let mainAdress = {
+const adressInput = document.querySelector('#address');
+const mainAdress = {
   lat: 35.6895,
   lng: 139.692,
 };
@@ -17,18 +17,15 @@ let mainAdress = {
  * @param {*} adress - adress data
  * @returns - string with lat. and lng. values
  */
-const fillInputValue = (adress) =>
-  `${Object.keys(adress)[0]}: ${adress.lat}, ${Object.keys(adress)[1]}:${
-    adress.lng
-  }`;
+const fillInputValue = (adress) => `${Object.keys(adress)[0]}: ${adress.lat}, ${Object.keys(adress)[1]}:${adress.lng}`;
 
 adressInput.value = fillInputValue(mainAdress);
 
 /**
  * Leaflet map setup
  */
-const map = L.map("map-canvas")
-  .on("load", () => {
+const map = L.map('map-canvas')
+  .on('load', () => {
     enableForms();
   })
   .setView(
@@ -38,20 +35,17 @@ const map = L.map("map-canvas")
     },
     13
   );
-L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-  {
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
-  }
-).addTo(map);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+  maxZoom: 20,
+  attribution:
+    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+}).addTo(map);
 
 /**
  * Main pin setup
  */
 const mainPinIcon = L.icon({
-  iconUrl: "./img/main-pin.svg",
+  iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -67,13 +61,10 @@ const mainPinMarker = L.marker(
   }
 );
 
-mainPinMarker.on("moveend", (evt) => {
+mainPinMarker.on('moveend', (evt) => {
   const updatedMainAdress = evt.target.getLatLng();
 
-  Object.keys(updatedMainAdress).forEach(
-    (key) =>
-      (updatedMainAdress[key] = Number(updatedMainAdress[key].toFixed(5)))
-  );
+  Object.keys(updatedMainAdress).forEach((key) => (updatedMainAdress[key] = Number(updatedMainAdress[key].toFixed(5))));
   adressInput.value = fillInputValue(updatedMainAdress);
 });
 
@@ -83,7 +74,7 @@ mainPinMarker.addTo(map);
  * Regular pin setup
  */
 const regularPinIcon = L.icon({
-  iconUrl: "./img/pin.svg",
+  iconUrl: './img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
